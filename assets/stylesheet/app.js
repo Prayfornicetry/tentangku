2473523a1303b938dfe51efd22751094a7bac35e
Prayfornicetry <fellyokss25@gmail.com>
2025-02-08 09:10:40 +0700

    Update app.js
    okey

--- assets/stylesheet/app.js
+++ assets/stylesheet/app.js
@@ -7,3 +7,11 @@ document.addEventListener("scroll", () => {
     navbar.classList.remove("scrolled");
   }
 });
+function hamburg(){
+    const navbar = document.querySelector(".dropdown")
+    navbar.style.transform = "translateY(0px)"
+}
+function cancel(){
+    const navbar = document.querySelector(".dropdown")
+    navbar.style.transform = "translateY(-500px)"
+}
